d643605d1153af6b2129b695a0de492faff1350b
abhishek rampi <abhishekabhi347@gmail.com>
2021-11-27 18:06:03 +0530

    Todo basic complition

---
+++ app/services/todo-data.js
@@ -0,0 +1,41 @@
+import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+class Todo {
+  @tracked text = '';
+  @tracked isCompleted = false;
+
+  constructor(text) {
+    this.text = text;
+  }
+}
+export default class TodoDataService extends Service {
+  @tracked todos = [];
+  get all() {
+    return this.todos;
+  }
+  get incomplete() {
+    return this.todos.filter((todo) => todo.isCompleted === false);
+  }
+  // get completed(){
+  //   return this.todos.filter(todo => todo.isCompleted ===true);
+  // }
+  get todoCountIsOne() {
+    return this.incomplete.length <= 1;
+  }
+
+  @action
+  clearCompleted() {
+    this.todos = this.incomplete;
+  }
+  @action
+  add(text) {
+    let newTodo = new Todo(text);
+    this.todos = [...this.todos, newTodo];
+  }
+  @action
+  toggleCompletion(todo){
+    todo.isCompleted = !todo.isCompleted;
+  }
+}
